c52d5aa0c1ab38cb2c40d2fd53cb87088fa138ec
Sujoy chakraborty <sujoyc070@gmail.com>
2023-07-20 23:32:09 +0600

    add to cart

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Add to cart JS //
+let shop_cart = document.querySelector('.shop-cart')
+
+// let cartArr = []
+
+
+cartArr.push
+function addToCart(){
+    if(shop_cart.innerHTML == 0){
+        shop_cart.innerHTML = parseInt(shop_cart.innerHTML) + 1
+    }
+}
